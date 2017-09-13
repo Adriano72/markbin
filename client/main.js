05d35669aa8b163b72861ee21d0e20187c1328d9
Adriano Massi <adrianomassi@gmail.com>
2017-09-14 01:09:35 +0200

    Lesson 90

--- client/main.js
+++ client/main.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/app';
 import { Bins } from '../imports/collections/bins';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>
+)
 
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.render-target'));
